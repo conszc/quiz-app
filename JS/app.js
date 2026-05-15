a50dfc150b147816b8d3a876ce10deddd1c9aa9c
conszc <conszc@farmingdale.edu>
2026-05-15 01:12:29 -0400

    Update quiz questions and answers in app.js

--- JS/app.js
+++ JS/app.js
@@ -1,33 +1,106 @@
 const quizData = [
+const questions = [
     {
-        question: "What does HTML stand for?",
+        question: "According to the Bible, Moses and Aaron had a sister named what?",
         answers: [
-            "HyperText Markup Language",
-            "HighText Machine Language",
-            "Hyper Transfer Markup Language",
-            "Home Tool Markup Language"
+                 "Jochebed", 
+                 "Ruth", 
+                 "Leah", 
+                 "Miriam"
         ],
-        correct: 0
+        correct: 4
+
     },
     {
-        question: "Which language is used for styling websites?",
+        question: "According to the children's nursery rhyme, what type of ocean did Columbus sail in 1492?",
         answers: [
-            "HTML",
-            "Python",
-            "CSS",
-            "Java"
+                "calm",
+                "blue",
+                "windy",
+                "really big"
+
+        ],
+        correct: 1
+    },
+    {
+        question: "Astronaut John Glenn served as a pilot in what branch of the military?",
+        answers: [
+                "Army",
+                "Air Force",
+                "Marines",
+                "Navy"
+        ],
+        correct: 3
+    },
+    {
+        question: "Backgammon is a how many player game?",
+        answers: [
+                "Two",
+                "Three",
+                "Four",
+                "Six"
+        ],
+        correct: 1
+    },
+    {
+        question: "Elephant tusks are made of what material?",
+        answers: [
+                "coral",
+                "ivory",
+                "bone",
+                "calcium"
+        ],
+        correct: 2  
+    },
+    {
+        question: "For a man and woman on a date, 'dutch treat' means what?",
+        answers: [
+                "the man pays",
+                "the woman pays",
+                "the Dutch pay",
+                "each pays their own way"
+        ],
+        correct: 4
+    },
+    {
+        question: "For what purpose would you use an awl?",
+        answers: [
+            "to shoot ducks",
+            "to polish floors",
+            "to make holes",
+            "to weigh fruit"
+        ],
+        correct: 3
+    },
+    {
+        question: "From what language does the term 'R.S.V.P.' originate?",
+        answers: [
+            "Russian",
+            "Italian",
+            "Portuguese",
+            "French"
+        ],
+        correct: 4
+    },
+    {
+        question: "From whom does the Lutheran Church get its name?",
+        answers: [
+            "Martin Luther King Jr",
+            "Martin Luther",
+            "Luther Vandross",
+            "Lex Luthor"
         ],
         correct: 2
     },
     {
-        question: "Which symbol is used for IDs in CSS?",
+        question: "Gerry Adams is the president of what organization?",
         answers: [
-            ".",
-            "#",
-            "@",
-            "$"
+            "Greenpeace",
+            "NASCAR",
+            "IBM 701",
+            "PLO"
         ],
-        correct: 1
+        correct: 3
     }
 ];
 
